a4f239bc0437d47151a362085df82d41aa546385
SaiSantanu <saisantanusahoo@gmail.com>
2026-08-08 22:22:56 +0530

    fix: correct CGPA value in About component

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -174,7 +174,7 @@ const About = () => {
                   school: "C. V Raman Global University",
                   location: "Odisha, India",
                   degree: "B.Tech in Computer Science (2023–2027)",
-                  grade: "CGPA: 8.24",
+                  grade: "CGPA: 8.2",
                 },
                 {
                   school: "ITAMATI HIGHER SECONDARY SCHOOL",
